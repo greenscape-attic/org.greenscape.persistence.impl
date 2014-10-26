534074529566ad1c710434952f8c391de24fe394
smsajid <smsajid786@gmail.com>
2014-10-26 06:50:32 +0530

    refactor based on core changes

--- src/org/greenscape/persistence/impl/PersistenceConfigurator.java
+++ src/org/greenscape/persistence/impl/PersistenceConfigurator.java
@@ -1,53 +1,41 @@
 package org.greenscape.persistence.impl;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
-import org.greenscape.persistence.ModelRegistryEntry;
-import org.greenscape.persistence.PersistenceConstants;
+import org.greenscape.core.ResourceEvent;
+import org.greenscape.core.ResourceRegistry;
+import org.greenscape.core.ResourceType;
 import org.greenscape.persistence.PersistenceService;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.ConfigurationPolicy;
-import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
 
-@Component(configurationPolicy = ConfigurationPolicy.REQUIRE, configurationPid = PersistenceConstants.CONFIG_NAME)
-public class PersistenceConfigurator {
-	private static final String PERSISTENCE_MODEL_AUTOCREATE = "persistence.model.autocreate";
-	private boolean autocreate;
-	private final List<ModelRegistryEntry> modelList = new ArrayList<>();
+@Component(property = { EventConstants.EVENT_TOPIC + "=" + ResourceRegistry.TOPIC_RESOURCE_REGISTERED,
+		EventConstants.EVENT_TOPIC + "=" + ResourceRegistry.TOPIC_RESOURCE_UNREGISTERED })
+public class PersistenceConfigurator implements EventHandler {
 	private PersistenceService persistenceService;
+
+	@Override
+	public void handleEvent(Event event) {
+		String name = (String) event.getProperty(ResourceEvent.RESOURCE_NAME);
+		String type = (String) event.getProperty(ResourceEvent.RESOURCE_TYPE);
+		if (type.equals(ResourceType.Model.name())) {
+			switch (event.getTopic()) {
+			case ResourceRegistry.TOPIC_RESOURCE_REGISTERED:
+				persistenceService.addModel(name);
+				break;
+			case ResourceRegistry.TOPIC_RESOURCE_UNREGISTERED:
+				break;
+			}
+		}
+	}
 
 	@Activate
 	private void activate(Map<String, Object> properties) {
-		String strAutocreate = (String) properties.get(PERSISTENCE_MODEL_AUTOCREATE);
-		autocreate = Boolean.valueOf(strAutocreate);
-		if (autocreate) {
-			createModel();
-		}
-	}
-
-	@Modified
-	private void modified(Map<String, Object> properties) {
-		String strAutocreate = (String) properties.get(PERSISTENCE_MODEL_AUTOCREATE);
-		autocreate = Boolean.valueOf(strAutocreate);
-		if (autocreate) {
-			createModel();
-		}
-	}
-
-	@Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC, policyOption = ReferencePolicyOption.GREEDY)
-	public void setModelRegistryEntry(ModelRegistryEntry entry) {
-		modelList.add(entry);
-	}
-
-	public void unsetModelRegistryEntry(ModelRegistryEntry entry) {
-		modelList.remove(entry);
 	}
 
 	@Reference(policy = ReferencePolicy.DYNAMIC)
@@ -57,11 +45,5 @@ public class PersistenceConfigurator {
 
 	public void unsetPersistenceService(PersistenceService persistenceService) {
 		this.persistenceService = null;
-	}
-
-	private void createModel() {
-		for (ModelRegistryEntry entry : modelList) {
-			persistenceService.addModel(entry.getModelName());
-		}
 	}
 }
